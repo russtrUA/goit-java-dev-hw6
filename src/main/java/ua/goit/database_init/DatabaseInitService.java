657b9b036478c10ca435fa765576101baffb8707
Ruslan Strynzha <russtr1984@gmail.com>
2024-09-12 20:08:53 +0300

    first commit

---
+++ src/main/java/ua/goit/database_init/DatabaseInitService.java
@@ -0,0 +1,29 @@
+package ua.goit.database_init;
+
+import ua.goit.exception.ConnectionException;
+import ua.goit.reader.SQLReader;
+
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static ua.goit.constant.Constant.CONNECTION_TROUBLE_MESSAGE;
+
+public class DatabaseInitService {
+    public static void main(String[] args){
+        String contentSql = new SQLReader().readContent(Path.of("./sql/init_db.sql"));
+        Connection connection = null;
+        try {
+            connection = DataBase.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new ConnectionException(CONNECTION_TROUBLE_MESSAGE);
+        }
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(contentSql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
